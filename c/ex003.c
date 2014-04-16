99bdb157fc6d8cb7058ef29122e889272111b8ed
aoisensi <aoisensi@aoisensi.info>
2014-04-17 03:11:31 +0900

    end of 2014-04-17-3h
    f**k c

---
+++ c/ex003.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+main()
+{
+	printf("私の名前は○○○\n");
+	printf("住所は○○○\n");
+	printf("趣味は○○○\n");
+}
---
+++ c/ex004.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+main()
+{
+	printf("答え=%d\n", 5 + 6);
+}
+
