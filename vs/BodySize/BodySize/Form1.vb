78edc377148003b9a35375060bfeb1dabc1d2695
aoisensi <aoisensi@aoisensi.info>
2014-05-07 12:11:19 +0900

    end of 2014-05-07-3h

---
+++ vs/BodySize/BodySize/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim hy, hi As Integer
+        hy = TextBox1.Text * TextBox1.Text * 22 / 10000
+        hi = (TextBox1.Text - hy) / hy * 100 & "%"
+        TextBox1.Text = hy
+        TextBox2.Text = hi
+        If hi <= -20 Then
+            Label3.Text = "やせすぎ"
+        ElseIf hi <= -10 Then
+            Label3.Text = "やせぎみ"
+        ElseIf hi <= 9 Then
+            Label3.Text = "標準"
+        ElseIf hi <= 19 Then
+            Label3.Text = "やや肥満"
+        Else
+            Label3.Text = "肥満"
+        End If
+        '糞助長でブチ切れそう
+    End Sub
+End Class
